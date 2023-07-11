aae37cfc9a6aebbfeafc122675c0d2e01d5ccb88
iamrealleader <iamrealleader@gmail.com>
2023-07-11 20:30:30 +0500

    fiximg issue

--- app/api/download/route.js
+++ app/api/download/route.js
@@ -5,6 +5,7 @@ export async function GET(req) {
  try{
   const { searchParams } = new URL(req.url);
   const link = searchParams.get('link');
+  console.log(link);
   const apiUrl = `https://api.threadsdownloader.io/load?url=${link}`;
   const response = await axios.get(apiUrl);
   const videoUrl = response.data.media[0].url;
